5060763428678adc9ccb4c12789d22c1977c733d
Jacob Cunningham <jacob.cunningham@live.com>
2019-04-25 13:39:34 -0500

    Added bash profile and bash aliases dotfile symlinks

---
+++ bash/.bash_profile
@@ -0,0 +1,7 @@
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+if [ -f ~/.docker_aliases ]; then
+    . ~/.docker_aliases
+fi
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 ln -sfv "$PWD/git/.gitconfig" ~
 ln -sfv "$PWD/git/.gitignore" ~
+ln -sfv "$PWD/bash/.bash_aliases" ~
+ln -sfv "$PWD/bash/.bash_profile" ~
 ln -sfv "$PWD/docker/.docker_aliases" ~
