318636846260bfd0292abe93d86c1774191c8641
Jacob Cunningham <jacob.cunningham@live.com>
2019-04-25 15:31:12 -0500

    added various commands to home bin

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -5,3 +5,8 @@ fi
 if [ -f ~/.docker_aliases ]; then
     . ~/.docker_aliases
 fi
+
+# will probably need to validate bin folder exists
+
+export PATH=$PATH:/Users/jacobcunningham/bin
+# export PATH=$PATH:$HOME/bin
--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ ln -sfv "$PWD/git/.gitignore" ~
 ln -sfv "$PWD/bash/.bash_aliases" ~
 ln -sfv "$PWD/bash/.bash_profile" ~
 ln -sfv "$PWD/docker/.docker_aliases" ~
+
+# validate ~/bin exists
+# Copy git/bin into ~/bin
+# chmod +x the files
+# Add ~/bin to $PATH
