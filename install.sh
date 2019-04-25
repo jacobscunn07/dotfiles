62ff4452b832dbc0e25f4a7d3da4730a1514f300
Jacob Cunningham <jacob.cunningham@live.com>
2019-04-25 17:39:32 -0500

    Added command installation function

--- install.sh
+++ install.sh
@@ -14,4 +14,22 @@ install_dotfiles () {
   done
 }
 
+install_commands () {
+  info 'Installing commands.'
+  BIN="$HOME/bin"
+
+  if [ ! -d "$BIN" ]; then
+    mkdir $BIN
+  fi
+
+  for src in $(find $PWD -path '*/bin/*' -type f)
+  do
+    base=$(basename $src)
+    dst="$BIN/$base"
+    cp $src $dst
+    chmod +x $dst
+  done
+}
+
 install_dotfiles
+install_commands
